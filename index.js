9e5c331a951f46bc8c9431baaf000e55ae311029
satendra-sr <satendra@coloredcow.com>
2022-09-20 15:49:14 +0530

    Cleanups

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const functions = require("@google-cloud/functions-framework");
 const { v2beta3 } = require("@google-cloud/tasks");
 
-
-
 functions.http("handle-webhook", (req, res)=> {
     console.log('Hello world!')
 });
