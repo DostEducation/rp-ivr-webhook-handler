00ed3f920afbdc4f4e54eeb37d5897c3cf05d8e5
satendra-sr <satendra@coloredcow.com>
2022-11-25 19:03:38 +0530

    final changes

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
 import functions_framework
 from google.cloud import tasks_v2
 import os
+import json
 
 SERVICE_ACCOUNT_EMAIL = os.environ.get("SERVICE_ACCOUNT_EMAIL")
 PROJECT = os.environ.get("GCP_PROJECT_NAME")
 QUEUE = os.environ.get("GCP_QUEUE_NAME")
 LOCATION = os.environ.get("GCP_RESOURCE_LOCATION")
 TARGET_FUNCTION_URL = os.environ.get("TARGET_FUNCTION_URL")
-TASK_NAME = os.environ.get("GCP_QUEUE_NAME") + "_task"
 
 @functions_framework.http
 def handle(req):
-    payload = req.get_json()
-    if payload is None:
-        return False
-        # handle this case
-
+    payload = json.dumps(req.get_json())
     client = tasks_v2.CloudTasksClient()
     parent = client.queue_path(PROJECT, LOCATION, QUEUE)
     task = {
-        "http_request": {
+        "http_request": { # Specify the type of request.
             "http_method": tasks_v2.HttpMethod.POST,
-            "url": TARGET_FUNCTION_URL,
-            "body": payload.encode(),
+            "url": TARGET_FUNCTION_URL, # The full url path that the task will be sent to.
+            "body": payload.encode('utf-8'),
             "headers": {"Content-type": "application/json"}
         }
     }
-    task["name"] = client.task_path(PROJECT, LOCATION, QUEUE, TASK_NAME)
 
     response = client.create_task(request={"parent": parent, "task": task})
-    return response
+    print('Response we got from queue');
+    print(response)
+    return "Success"
