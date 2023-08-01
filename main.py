7a2e1fdce2f3d227f720e93f98f49bc12bd23393
Sachinbisht27 <sachinbisht27@gmail.com>
2023-08-02 00:51:41 +0530

    Added different level of logs in the rp-ivr-webhook-handler service

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import functions_framework
 from google.cloud import tasks_v2
-import os
+from utils import logger
+import config
 import json
 
-SERVICE_ACCOUNT_EMAIL = os.environ.get("SERVICE_ACCOUNT_EMAIL")
-PROJECT = os.environ.get("GCP_PROJECT_NAME")
-QUEUE = os.environ.get("GCP_QUEUE_NAME")
-LOCATION = os.environ.get("GCP_RESOURCE_LOCATION")
-TARGET_FUNCTION_URL = os.environ.get("TARGET_FUNCTION_URL")
 
 @functions_framework.http
 def handle(req):
-    payload = json.dumps(req.get_json())
-    client = tasks_v2.CloudTasksClient()
-    parent = client.queue_path(PROJECT, LOCATION, QUEUE)
-    task = {
-        "http_request": { # Specify the type of request.
-            "http_method": tasks_v2.HttpMethod.POST,
-            "url": TARGET_FUNCTION_URL, # The full url path that the task will be sent to.
-            "body": payload.encode('utf-8'),
-            "headers": {"Content-type": "application/json"}
-        }
-    }
+    try:
+        payload = json.dumps(req.get_json())
+        logger.info(f"Payload: {payload}")
 
-    response = client.create_task(request={"parent": parent, "task": task})
-    print('Response we got from queue');
-    print(response)
-    return "Success"
+        client = tasks_v2.CloudTasksClient()
+        parent = client.queue_path(config.PROJECT, config.LOCATION, config.QUEUE)
+        task = {
+            "http_request": { # Specify the type of request.
+                "http_method": tasks_v2.HttpMethod.POST,
+                "url": config.TARGET_FUNCTION_URL, # The full url path that the task will be sent to.
+                "body": payload.encode('utf-8'),
+                "headers": {"Content-type": "application/json"}
+            }
+        }
+
+        response = client.create_task(request={"parent": parent, "task": task})
+        logger.info(f"Response from Cloud Tasks API: {response}")
+
+        return "Success"
+    except Exception as e:
+        logger.error(f"Error in handle function: {e}")
