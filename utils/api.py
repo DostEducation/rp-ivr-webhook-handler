2753a39c38addef0d320429f7046984fe47d25c3
Sachinbisht27 <sachinbisht27@gmail.com>
2023-08-15 16:43:21 +0530

    Implemented the api token autherization in the service

---
+++ utils/api.py
@@ -0,0 +1,17 @@
+from flask import request
+from functools import wraps
+import config
+from utils import logger
+
+def authenticate_api_token(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        try:
+            api_token = request.headers.get('Authorization')
+            if api_token != config.AUTH_TOKEN:
+                logger.error(f"Invalid API token {api_token} recived in the request.")
+                return {'error': 'Invalid API token'}, 401
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.error(f"Exception occured while authenticating the api token. Error message: {e}")
+    return decorated
