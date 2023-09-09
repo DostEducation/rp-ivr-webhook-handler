60ffacc5be5845a0eda2d0f7e4f40bb95035efc1
Sachinbisht27 <sachinbisht27@gmail.com>
2023-09-09 14:38:54 +0530

    Added payload in the logs when the apitoken doesn't authenticate

--- utils/auth.py
+++ utils/auth.py
@@ -1,6 +1,7 @@
 from flask import request
 from functools import wraps
 import config
+import json
 from utils import logger
 
 def authenticate_api_token(func):
@@ -8,8 +9,9 @@ def authenticate_api_token(func):
     def decorated(*args, **kwargs):
         try:
             api_token = request.headers.get('Authorization')
+            payload = json.dumps(request.get_json())
             if api_token != config.AUTH_TOKEN:
-                logger.error(f"Invalid API token {api_token} received in the request.")
+                logger.error(f"Invalid API token {api_token} received in the request from payload {payload}.")
                 return {'error': 'Invalid API token'}, 401
             return func(*args, **kwargs)
         except Exception as e:
